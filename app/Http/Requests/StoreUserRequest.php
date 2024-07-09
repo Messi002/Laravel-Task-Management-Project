cf4390e1b9ed10d52ad84e37f301b64162853cba
Messi002 <rostandmessi2@gmail.com>
2024-07-09 14:30:00 +0100

    added logic for task-resource-reqests

--- app/Http/Requests/StoreUserRequest.php
+++ app/Http/Requests/StoreUserRequest.php
@@ -22,10 +22,10 @@ class StoreUserRequest extends FormRequest
      */
     public function rules(): array
     {
-        return [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email'],
-            'password' => ['required', 'confirmed' ,Password::min(8)->letters()->symbols()]
-        ];
+            return [
+                'name' => ['required', 'string', 'max:255'],
+                'email' => ['required', 'string', 'email', 'unique:users,email'],
+                'password' => ['required', 'confirmed' ,Password::min(8)->letters()->symbols()]
+            ];
     }
 }
